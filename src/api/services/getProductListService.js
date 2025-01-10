2a9e1052bdd00e8efde4a87282d4bee1ef6952b3
deepdeb <debnath.pradeep619@gmail.com>
2025-01-10 16:51:19 +0530

    modify getProductListService.js

--- src/api/services/getProductListService.js
+++ src/api/services/getProductListService.js
@@ -2,7 +2,7 @@ const readPool = require('../../config/mysql').readPool;
 
 exports.getProductList = async () => {
     try {
-        let sql = "select product_id, product_name, catch_phrase, product_image, product_description, price, availability, unit, package from product_list where is_deleted = 0"
+        let sql = "select pl.product_id, pl.product_name, pl.catch_phrase, pl.product_image, pl.product_description, pl.price, pl.availability, pl.unit, pl.package, o.offer_type, o.offer_amount, pl.price - (pl.price * (o.offer_amount / 100)) as final_price, (pl.price * (o.offer_amount / 100)) as difference from product_list as pl left join offers as o on o.product_id = pl.product_id where pl.is_deleted = 0 and o.is_active = 1"
 
         const [resp] = await readPool.query(sql);
 
