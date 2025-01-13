bd8c8663cb33d6c94264d5f76c9ea70bd5274aa0
deepdeb <debnath.pradeep619@gmail.com>
2025-01-13 18:38:10 +0530

    modify files

--- src/api/controllers/common/getProductList.js
+++ src/api/controllers/common/getProductList.js
@@ -4,10 +4,11 @@ const prisma = new PrismaClient();
 
 exports.getProductListController = async (req, res) => {
     try {
-        let productList = await prisma.product.findMany({})
-        return res.json({ success: true, status: 200, message: res.message, products: productList})
+        // let productList = await prisma.product.findMany({})
+        let productList = await prisma.$queryRaw`select pl.product_id, pl.product_name, pl.catch_phrase, pl.product_image, pl.product_description, pl.price, pl.availability, pl.unit, pl.package, o.offer_type, o.offer_amount, pl.price - (pl.price * (o.offer_amount / 100)) as final_price, (pl.price * (o.offer_amount / 100)) as difference from product as pl left join offers as o on o.product_id = pl.product_id where pl.is_deleted = 0`
+        return res.json({ success: true, status: 200, message: res.message, products: productList })
     } catch (error) {
         console.log('Get product list controller error: ', error);
-        return res.json({ success: false, status: 400, message: error, products: []})
+        return res.json({ success: false, status: 400, message: error, products: [] })
     }
 }
