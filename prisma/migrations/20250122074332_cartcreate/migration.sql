6acee9eeadb27ece8d8fe8c0fe723266e5f68e5e
deepdeb <debnath.pradeep619@gmail.com>
2025-01-22 13:46:03 +0530

    cart table created

---
+++ prisma/migrations/20250122074332_cartcreate/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `cart` (
+    `cart_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userUid` VARCHAR(191) NOT NULL,
+    `productList` JSON NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `cart_userUid_key`(`userUid`),
+    PRIMARY KEY (`cart_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
