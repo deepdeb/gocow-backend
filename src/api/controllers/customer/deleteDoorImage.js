1080fdc5a26dee2cff78780adac5e05b17593e3e
8UCK37 <aneekenduviking666@gmsil.com>
2025-01-22 15:44:01 +0530

    door image delete api

--- src/api/controllers/customer/deleteDoorImage.js
+++ src/api/controllers/customer/deleteDoorImage.js
@@ -1,11 +1,17 @@
 const path = require('path')
+const fs = require('fs')
 
 
 exports.deleteDoorImageController = async (req, res) => {
     try{
+        const fileName = req.user.user_id +"_door"+ '.' + 'jpg'
+        const filePath = path.join(__dirname, "../../../../public/customer_door/");
+        console.log(filePath+fileName)
+        fs.unlinkSync(filePath+fileName)
+        
         return res.json({ success: true, status: 200, message: 'Call Recieved' })
     }catch(error){
-        return res.json({ success: false, status: 400, message: 'Something went wrong'})
+        return res.json({ success: false, status: 400, message: error})
     }
     
 }
