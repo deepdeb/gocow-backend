a4caf0cde2d88ca9f1588bb2f8d02308302bd2e7
deepdeb <debnath.pradeep619@gmail.com>
2025-01-27 15:18:51 +0530

    modify schema.prisma

---
+++ prisma/migrations/20250127094746_modifyuser_info/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `milk_preferences` on the `user_info` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `user_info` DROP COLUMN `milk_preferences`;
