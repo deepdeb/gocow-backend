2b1d6d729200c406294a8c39eda4d0ac3ada2371
deepdeb <debnath.pradeep619@gmail.com>
2025-01-30 17:35:08 +0530

    create getProductListCustomer

--- src/api/controllers/common/getProductList.js
+++ src/api/controllers/common/getProductList.js
@@ -2,9 +2,9 @@ const Joi = require('joi');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-exports.getProductListController = async (req, res) => {
+exports.getProductListAdminController = async (req, res) => {
     try {
-        let productList = await prisma.$queryRaw`select pl.product_id, pl.product_name, pl.catch_phrase, pl.product_image, pl.product_description, pl.price, pl.availability, pl.unit, pl.package, o.offer_type, o.offer_amount, pl.price - (pl.price * (o.offer_amount / 100)) as final_price, (pl.price * (o.offer_amount / 100)) as difference from product as pl left join offers as o on o.product_id = pl.product_id where pl.is_deleted = 0`
+        let productList = await prisma.$queryRaw`select pl.product_id, pl.product_name, pl.catch_phrase, pl.product_image, pl.product_description, pl.price, pl.availability, pl.unit, pl.package, o.offer_type, o.offer_amount, pl.price - (pl.price * (o.offer_amount / 100)) as final_price, (pl.price * (o.offer_amount / 100)) as difference, pl.updated_at, pl.updated_by, pl.created_at, pl.created_by from product as pl left join offers as o on o.product_id = pl.product_id where pl.is_deleted = 0`
         return res.json({ success: true, status: 200, message: res.message, products: productList })
     } catch (error) {
         console.log('Get product list controller error: ', error);
--- src/api/routes/commonRoute.js
+++ src/api/routes/commonRoute.js
@@ -1,10 +1,11 @@
 const auth  = require('../../api/middleware/authMiddlewareCustomer')
 const ensureAuthenticated = auth.ensureAuthenticated
 const router = require('express').Router();
-const { getProductListController } = require('../controllers/common/getProductList');
+const { getProductListAdminController, getProductListCustomerController } = require('../controllers/common/getProductList');
 const { createOrder, adminGetOrderList, customerGetOrderList } = require('../controllers/common/salesOrder');
 module.exports = router;
-router.get('/getProductList', getProductListController)
+router.get('/getProductListAdmin', getProductListAdminController)
 router.post('/createOrder', ensureAuthenticated, createOrder)
 router.get('/getAdminOrderList', adminGetOrderList)
 router.get('/getCustomerOrderList', ensureAuthenticated, customerGetOrderList)
+router.get('/getProductListCustomer', getProductListCustomerController)
