12eddeff5cbde1adcf002a25954a96c9ca40dd45
deepdeb <debnath.pradeep619@gmail.com>
2025-02-04 13:54:43 +0530

    added orderby desc in customerManager.js

--- src/api/controllers/admin/customerManager.js
+++ src/api/controllers/admin/customerManager.js
@@ -3,7 +3,11 @@ const prisma = new PrismaClient();
 
 exports.adminReadCustomer = async (req, res) => {
     try{
-        let customer_list = await prisma.customer.findMany({})
+        let customer_list = await prisma.customer.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
         return res.json({ success: true, status: 200 ,customerList:customer_list })
     }catch(error){
         console.log('Customer table read error ', error);
