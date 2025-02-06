95cea38a68d09b45264c484ffa986070b3fd1736
8UCK37 <aneekenduviking666@gmail.com>
2025-02-06 15:50:47 +0530

    fix?

--- src/index.js
+++ src/index.js
@@ -9,19 +9,24 @@ const socketRunner = require('../src/api/middleware/socketRunner')
 const http = require("http");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
+// const io = require('socket.io')(http, {
+//     cors: {
+//       origins: ['http://localhost:4200']
+//     }
+//   });
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // Adjust based on frontend domain
-    methods: ["GET", "POST"]
+    origins: "*", // Adjust based on frontend domain
+    methods: ["GET", "POST"],
   }
 });
 
-
+// app.listen(port, 192.168.1.6, () => console.log(`server listening on port ${port} || 3000(${env})`));
 socketRunner.execute(io)
-
+// http.listen(5000, () => console.log(`Listening on port ${3000}`));
+// app.listen(port, () => console.log(`server listening on port ${port} || 3000(${env})`));
 server.listen(port, () => console.log(`Server running on port ${port}`));
-
 app.set('socketIo',io)
 app.get('/test',async function (req, res)  {
     console.log('test api called');
