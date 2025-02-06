07e6874ad1fda26e07a5d1e0ef811a6c5b0aef19
8UCK37 <aneekenduviking666@gmail.com>
2025-02-06 15:46:39 +0530

    forced websocket to use same port

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ const app = require('./config/express');
 const { port, env } = require('./config/vars');
 const socketRunner = require('../src/api/middleware/socketRunner')
 
-const http = require('http').createServer(app);
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 
-const io = require('socket.io')(http, {
-    cors: {
-      origins: ['http://localhost:4200']
-    }
-  });
-// app.listen(port, 192.168.1.6, () => console.log(`server listening on port ${port} || 3000(${env})`));
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Adjust based on frontend domain
+    methods: ["GET", "POST"]
+  }
+});
+
+
 socketRunner.execute(io)
-http.listen(5000, () => console.log(`Listening on port ${3000}`));
-app.listen(port, () => console.log(`server listening on port ${port} || 3000(${env})`));
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
 app.set('socketIo',io)
 app.get('/test',async function (req, res)  {
     console.log('test api called');
