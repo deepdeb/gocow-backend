618dcdfcadb5c58cc43780678ebd4571e3e1545c
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2025-02-12 22:16:28 +0530

    workaround using singular port

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 require('dotenv').config();
-const auth  = require('./api/middleware/authMiddlewareCustomer')
+const auth  = require('./api/middleware/authMiddlewareCustomer');
 
-const ensureAuthenticated = auth.ensureAuthenticated
+const ensureAuthenticated = auth.ensureAuthenticated;
 const app = require('./config/express');
 const { port, env } = require('./config/vars');
-const socketRunner = require('../src/api/middleware/socketRunner')
+const socketRunner = require('../src/api/middleware/socketRunner');
 
-const http = require('http').createServer(app);
+const http = require('http').createServer(app); // Creating an HTTP server with Express
 
 const io = require('socket.io')(http, {
     cors: {
-      origins: ['http://localhost:4200']
+        origins: ['http://localhost:4200']
     }
-  });
-// app.listen(port, 192.168.1.6, () => console.log(`server listening on port ${port} || 3000(${env})`));
-socketRunner.execute(io)
-http.listen(5000, () => console.log(`Listening on port ${3000}`));
-app.listen(port, () => console.log(`server listening on port ${port} || 3000(${env})`));
-app.set('socketIo',io)
-app.get('/test',async function (req, res)  {
-    console.log('test api called');
-    res.send('test api called');
-})
+});
+
+socketRunner.execute(io);
+
+// Using the same port for both Express and WebSocket
+http.listen(port, () => console.log(`Server listening on port ${port} (${env})`));
+
+app.set('socketIo', io);
+
+
+app.get('/test', async function (req, res) {
+    console.log('Test API called');
+    res.send('Test API called');
+});
 
 module.exports = app;
