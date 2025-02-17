6da2160f9435bee0a7e50bd2e2de35bfe681907e
deepdeb <debnath.pradeep619@gmail.com>
2025-02-17 14:01:37 +0530

    admin create offer API done

--- src/api/controllers/admin/offerManager.js
+++ src/api/controllers/admin/offerManager.js
@@ -4,19 +4,20 @@ const prisma = new PrismaClient();
 exports.adminCreateOffer = async (req, res) => {
     try {
 
-        console.log('req body>>>>', req.body)
+        // console.log('req body>>>>', req.body)
 
         let admin_create_offer = await prisma.offer.create({
             data: {
-                admin_id: req.body.admin_id,
-                type: req.body.type,
-                category: req.body.category,
-                discount_type: req.body.discount_type,
+                admin_id: req.admin.id,
+                offer_label: req.body.label,
+                type: req.body.offerType,
+                category: req.body.offerCategory,
+                discount_type: req.body.offerDisc,
                 discount_value: req.body.discount_value,
-                buy_quantity: req.body.category === "BUY_N_GET_X" ? req.body.buy_quantity : undefined,
-                get_quantity: req.body.category === "BUY_N_GET_X" ? req.body.get_quantity : undefined,
-                from_date: req.body.from_date,
-                to_date: req.body.to_date,
+                buy_quantity: req.body.offerCategory === "BUY_N_GET_X" ? req.body.buy_quantity : undefined,
+                get_quantity: req.body.offerCategory === "BUY_N_GET_X" ? req.body.get_quantity : undefined,
+                from_date: req.body.startDate,
+                to_date: req.body.endDate,
                 offer_products: req.body.type === "PRODUCT" && req.body.product_ids.length > 0 ? {
                     create: req.body.product_ids.map(product_id => ({ product: { connect: { product_id } } }))
                 } : undefined,
@@ -38,11 +39,10 @@ exports.adminGetOffer = async (req, res) => {
                     include: {
                         product: true
                     }
-                }
+                },
+                admin: true
             }
         })
-
-        console.log('offer list>>>>', offer_list)
 
         return res.json({ success: true, status: 200, offer_list: offer_list })
     } catch (error) {
